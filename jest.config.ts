3eefa179a3e4984dc87b1efcf51505c95c6503a5
thgee <thgeee@naver.com>
2025-02-05 11:09:53 +0900

    chore: Jest 환경설정 추가

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default createJestConfig(config);
